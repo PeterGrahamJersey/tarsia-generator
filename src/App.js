2375af903bbf360029f5ff8853a69055443d2106
Peter Graham <petergrahamjersey@gmail.com>
2020-10-11 20:33:51 +0100

    triangle placement

--- src/App.js
+++ src/App.js
@@ -2,6 +2,31 @@ import React from 'react';
 //import QuestionAnswer from './QuestionAnswer.js'
 //import logo from './logo.svg';
 import './App.css';
+import Triangle from './Components';
+
+function triangle(row, n, side) {
+  const h = Math.sqrt(3)*side/2
+  return <>
+      <polygon 
+        points={"0,0 "+side+",0 "+side/2+","+h}
+        style={{
+          fill:"lime",
+          stroke:"purple",
+          strokeWidth:1}} 
+      />
+    </>
+}
+
+function PairedText(row, n, side, rotation, value1, value2) {
+  const h = Math.sqrt(3)*side/2
+  const x = side + side/2
+  const y = h/2
+  const cor = x + "," + y
+  return <>
+      <text transform={"rotate("+rotation+" "+cor+")"} x={x} y={y} fill="red" textLength={side}>{value1}</text>
+      <text transform={"rotate("+rotation+180+" "+cor+")"} x={x} y={y} fill="red" textLength={side}>{value2}</text>
+    </>
+}
 
 class App extends React.Component {
   constructor(props) {
@@ -24,10 +49,6 @@ class App extends React.Component {
   }
 
   render() {
-    //const hex_r = 1;
-    //const hex_s = 1;
-    //const hex_h = Math.sqrt(Math.hex_s^2 - hex_r^2);
-
     return (
       <div className="App">
         <header className="App-header">
@@ -47,15 +68,15 @@ class App extends React.Component {
             </label>
           </form>
           <p>Text output: {this.state.Q1}</p>
-          <svg height="360" width="360">
-            <polygon 
-              points="90,0 270,0 360,180 270,360 90,360 0,180" 
-              style={{
-                fill:"lime",
-                stroke:"purple",
-                strokeWidth:1}} 
-            />
-            <text x="90" y="15" fill="red" textLength="180">{this.state.Q1}</text>
+          <svg height="600" width="600">
+            <Triangle row="1" col="2" />
+            <Triangle row="1" col="3" />
+            <Triangle row="1" col="3" />
+            <Triangle row="1" col="3" />
+            <Triangle row="1" col="3" />
+            <Triangle row="2" col="1" />
+            <Triangle row="2" col="2" />
+            {PairedText(1, 1, 100, 0, this.state.Q1, this.state.A1)}
           </svg>
         </header>
     </div>
---
+++ src/config.js
@@ -0,0 +1,7 @@
+const hexConfig = [
+    {row: 1, col: 2, values: ["5q", "1q", null]},
+    {row: 1, col: 3, values: [null, "1a", "2q"]},
+    {row: 1, col: 4, values: ["6q", "3q", "2a"]},
+    {row: 1, col: 5, values: [null, "3a", "4q"]},
+    {row: 1, col: 6, values: ["7q", null, "4a"]}
+]
