d65947233c7a7a029ba5a04b84fb747c48257109
Dale Blackburn <dale@dakebl.co.uk>
2020-10-18 21:55:59 +0100

    move TarsiaGrid component from class component to function component

--- src/components/TarsiaGrid/index.jsx
+++ src/components/TarsiaGrid/index.jsx
@@ -3,42 +3,31 @@ import Triangle from '../Triangle'
 
 import appConfig from '../../config'
 
-class TarsiaGrid  extends React.Component {
-  constructor(props) {
-    super(props);
-    this.id = props.id;
-    this.config = props.config
-    this.values = props.values
-  }
-
-  render() {
-    const grid = []
-    for (let tri of this.config) {
-      let valueArray = []
-      for (let valueCode of tri.values) { // lookup the value code from the config
-        if (valueCode) { // handles nulls
-          let valueCodeSplit = [valueCode.substr(0,valueCode.length-1), valueCode.slice(-1)] // split into number and q/a
-          if (this.values[valueCodeSplit[0]]) { // handles nulls
-            valueArray.push(this.values[valueCodeSplit[0]][valueCodeSplit[1]]) // add the value based on the question number and q/a
-          } else (
-            valueArray.push(valueCode)
-          )
-        } else {
-          valueArray.push(null) // null handling
-        }
+const TarsiaGrid = ({ id, config, values }) => {
+  const grid = []
+  for (let tri of config) {
+    let valueArray = []
+    for (let valueCode of tri.values) { // lookup the value code from the config
+      if (valueCode) { // handles nulls
+        let valueCodeSplit = [valueCode.substr(0,valueCode.length-1), valueCode.slice(-1)] // split into number and q/a
+        if (values[valueCodeSplit[0]]) { // handles nulls
+          valueArray.push(values[valueCodeSplit[0]][valueCodeSplit[1]]) // add the value based on the question number and q/a
+        } else (
+          valueArray.push(valueCode)
+        )
+      } else {
+        valueArray.push(null) // null handling
       }
-      grid.push(<Triangle row={tri.row} col={tri.col} values={valueArray} config={appConfig.triangle} />)
     }
-
-    return (
-      <g>
-        {grid}
-      </g>
-    )
+    grid.push(<Triangle row={tri.row} col={tri.col} values={valueArray} config={appConfig.triangle} />)
   }
+
+  return (
+    <g>
+      {grid}
+    </g>
+  )
 }
-
-
 
 export default TarsiaGrid
 
