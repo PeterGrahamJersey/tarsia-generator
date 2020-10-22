b76e8515b287dd2662d9d2c777ec6325d7b53f47
Peter Graham <petergrahamjersey@gmail.com>
2020-10-22 14:08:08 +0100

    separating value lookup logic in TarsiaGrid

--- src/components/TarsiaGrid/index.jsx
+++ src/components/TarsiaGrid/index.jsx
@@ -3,21 +3,18 @@ import Triangle from '../Triangle'
 import appConfig from '../../config'
 
 const TarsiaGrid = ({id, config, questions, answers}) => {
-  const grid = []
-  for (let triangle of config) {
-    let valueArray = []
-    for (let valueCode of triangle.values) { // lookup the value code from the config
+  
+  const getTriangleValues = ({triangleValueCodeArray, questions, answers}) => triangleValueCodeArray.map(valueCode => {
       if (valueCode) { // handles nulls
         let [questionNumber, questionOrAnswer] = [valueCode.substr(0,valueCode.length-1), valueCode.slice(-1)] // split into number and q/a
         const values = questionOrAnswer === 'q' ? questions : answers
         const value = values[questionNumber] ? values[questionNumber] : valueCode
-        valueArray.push(value) // add the value based on the question number and q/a
-      } else {
-        valueArray.push(null) // null handling
+        return value
       }
-    }
-    grid.push(<Triangle row={triangle.row} col={triangle.col} values={valueArray} config={appConfig.triangle} key={`${id}-${triangle.row}-${triangle.col}`}/>)
-  }
+      return null
+    })
+
+  const grid = config.map(triangle => <Triangle row={triangle.row} col={triangle.col} values={getTriangleValues({triangleValueCodeArray:triangle.values, questions, answers})} config={appConfig.triangle} key={`${id}-${triangle.row}-${triangle.col}`}/>)
 
   return (
     <g>
