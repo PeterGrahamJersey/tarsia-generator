50a12e191bcd56ed87199f316e9a4a1837795932
Peter Graham <petergrahamjersey@gmail.com>
2020-10-26 15:11:34 +0000

    correcting duplicated 4q in triangle grid

--- src/data/grids/triangleGrid.js
+++ src/data/grids/triangleGrid.js
@@ -4,7 +4,7 @@ export default [
   {location: {row:2, col:4}, values: ['1a', '2a', '3q']},
   {location: {row:2, col:5}, values: ['5q', null, '3a']},
   {location: {row:3, col:2}, values: ['10q', '6q', null]},
-  {location: {row:3, col:3}, values: ['4q', '6a', '7q']},
+  {location: {row:3, col:3}, values: ['4a', '6a', '7q']},
   {location: {row:3, col:4}, values: ['11q', '8q', '7a']},
   {location: {row:3, col:5}, values: ['5a', '8a', '9q']},
   {location: {row:3, col:6}, values: ['12q', null, '9a']},
