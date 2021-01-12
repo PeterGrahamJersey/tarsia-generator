b890b6c94a6f5597fd70969492348df7ad48212a
Peter Graham <petergrahamjersey@gmail.com>
2021-01-12 08:39:59 +0000

    tweaks

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -44,9 +44,9 @@ const App = (id) => {
       const formattedSaveCode = pdf.splitTextToSize(saveCode, appConfig.pdf.width-appConfig.pdf.printMargin*2)
       pdf.addPage({orientation:'l', format:'a4'})
       pdf.text(
-        'To edit your tarsia, go to tarsiamaker.co.uk click load and paste this code:',
+        'To edit your tarsia, go to www.tarsiamaker.co.uk, click load and paste this code:',
         appConfig.pdf.printMargin, //x
-        appConfig.pdf.printMargin  //y
+        appConfig.pdf.printMargin + 5  //y
       )
       pdf.text(
         formattedSaveCode,
@@ -54,17 +54,19 @@ const App = (id) => {
         appConfig.pdf.printMargin + 15  //y
       )
     }
-    const addNextSvgToPdf = (pdf, page, pages, svgsToExport, saveCode) => {
+    const addNextSvgToPdf = (pdf, page, pages, svgsToExport, textToExport, saveCode) => {
       // Recursive, adds an svg, waits for it to finish, then adds the next one until saving
       // Get svg
       let svgToExport = svgsToExport[page]
+      let text = textToExport[page]
       // Add to pdf
+      pdf.text(text, appConfig.pdf.printMargin, appConfig.pdf.printMargin+5)
       pdf.svg(svgToExport,{x:5,y:5,width:287,height:200}).then(() => {
         page = page+1
         if (page !== pages) {
           // iterate
           pdf.addPage({orientation:'l', format:'a4'})
-          addNextSvgToPdf(pdf, page, pages, svgsToExport, saveCode)
+          addNextSvgToPdf(pdf, page, pages, svgsToExport, textToExport, saveCode)
         } else {
           addSaveCodeToPdf(pdf, saveCode)
           pdf.save('tarsia.pdf')
@@ -75,12 +77,15 @@ const App = (id) => {
     const previewSvg = document.getElementById('previewSvg')
     const printSvgs = document.getElementById('printSvgDiv').children
     var svgsToExport = []
+    var textToExport = []
     svgsToExport.push(previewSvg)
+    textToExport.push('Solution:')
     for (const printSvg of printSvgs) {
       svgsToExport.push(printSvg)
+      textToExport.push('Print and cut out:')
     }
     const svgPages = svgsToExport.length
-    addNextSvgToPdf(pdf, 0, svgPages, svgsToExport, saveCode)
+    addNextSvgToPdf(pdf, 0, svgPages, svgsToExport, textToExport, saveCode)
   };
 
   const generateSaveCode = () => {
