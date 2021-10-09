a143bf318e15aa3b681d9b6758ffed8e60f2e4fe
Peter Graham <peter.graham@15gifts.com>
2021-10-09 23:45:53 +0100

    moving export out of app

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,6 +1,5 @@
 import React, {useState} from 'react';
 import { Helmet } from 'react-helmet';
-import jsPDF from 'jspdf';
 import 'svg2pdf.js';
 import './App.css';
 
@@ -13,10 +12,9 @@ import PreviewSvg from '../PreviewSvgDiv';
 import gridIcons from '../../data/gridIcons'
 import GridIcon from '../GridIcon'
 import {ClearModal, LoadModal, SaveModal} from '../Modal'
-import appConfig from '../../data/config';
 
 // Functions
-import {generateSaveCode, parseSaveCode} from '../../utils/saveLoadExport.js'
+import {generateSaveCode, parseSaveCode, generateAndSavePdf} from '../../utils/saveLoadExport'
 
 const App = (id) => {
   const [questions, setQuestions] = useState({})
@@ -38,57 +36,10 @@ const App = (id) => {
   }
 
   const exportToPdf = () => {
-    //Initialise pdf
-    const pdf = new jsPDF({
-      orientation: 'landscape',
-      unit:'mm'
-    });
-    const addSaveCodeToPdf = (pdf, saveCode) => {
-      const formattedSaveCode = pdf.splitTextToSize(saveCode, appConfig.pdf.width-appConfig.pdf.printMargin*2)
-      pdf.addPage({orientation:'l', format:'a4'})
-      pdf.text(
-        'To edit your tarsia, go to www.tarsiamaker.co.uk, click load and paste this code:',
-        appConfig.pdf.printMargin, //x
-        appConfig.pdf.printMargin + 5  //y
-      )
-      pdf.text(
-        formattedSaveCode,
-        appConfig.pdf.printMargin, //x
-        appConfig.pdf.printMargin + 15  //y
-      )
-    }
-    const addNextSvgToPdf = (pdf, page, pages, svgsToExport, textToExport, saveCode) => {
-      // Recursive, adds an svg, waits for it to finish, then adds the next one until saving
-      // Get svg
-      let svgToExport = svgsToExport[page]
-      let text = textToExport[page]
-      // Add to pdf
-      pdf.text(text, appConfig.pdf.printMargin, appConfig.pdf.printMargin+5)
-      pdf.svg(svgToExport,{x:5,y:5,width:287,height:200}).then(() => {
-        page = page+1
-        if (page !== pages) {
-          // iterate
-          pdf.addPage({orientation:'l', format:'a4'})
-          addNextSvgToPdf(pdf, page, pages, svgsToExport, textToExport, saveCode)
-        } else {
-          addSaveCodeToPdf(pdf, saveCode)
-          pdf.save('tarsia.pdf')
-        }
-      })
-    }
     const saveCode = generateSaveCode(questions, answers, grid)
     const previewSvg = document.getElementById('previewSvg')
     const printSvgs = document.getElementById('printSvgDiv').children
-    var svgsToExport = []
-    var textToExport = []
-    svgsToExport.push(previewSvg)
-    textToExport.push('Solution:')
-    for (const printSvg of printSvgs) {
-      svgsToExport.push(printSvg)
-      textToExport.push('Print and cut out:')
-    }
-    const svgPages = svgsToExport.length
-    addNextSvgToPdf(pdf, 0, svgPages, svgsToExport, textToExport, saveCode)
+    generateAndSavePdf(saveCode, previewSvg, printSvgs)
   };
 
   const saveToText = () => {
--- src/utils/saveLoadExport.js
+++ src/utils/saveLoadExport.js
@@ -1,7 +1,7 @@
 import jsPDF from 'jspdf';
 import LZString from 'lz-string'
 import 'svg2pdf.js';
-
+import appConfig from '../data/config';
 
 // Save to string
 const generateSaveCode = (questions, answers, grid) => {
@@ -25,59 +25,61 @@ const parseSaveCode = (text) => {
   }
 };
 
-// // Export to PDF
-// const exportToPdf = () => {
-//   //Initialise pdf
-//   const pdf = new jsPDF({
-//     orientation: 'landscape',
-//     unit:'mm'
-//   });
-//   const addSaveCodeToPdf = (pdf, saveCode) => {
-//     const formattedSaveCode = pdf.splitTextToSize(saveCode, appConfig.pdf.width-appConfig.pdf.printMargin*2)
-//     pdf.addPage({orientation:'l', format:'a4'})
-//     pdf.text(
-//       'To edit your tarsia, go to www.tarsiamaker.co.uk, click load and paste this code:',
-//       appConfig.pdf.printMargin, //x
-//       appConfig.pdf.printMargin + 5  //y
-//     )
-//     pdf.text(
-//       formattedSaveCode,
-//       appConfig.pdf.printMargin, //x
-//       appConfig.pdf.printMargin + 15  //y
-//     )
-//   }
-//   const addNextSvgToPdf = (pdf, page, pages, svgsToExport, textToExport, saveCode) => {
-//     // Recursive, adds an svg, waits for it to finish, then adds the next one until saving
-//     // Get svg
-//     let svgToExport = svgsToExport[page]
-//     let text = textToExport[page]
-//     // Add to pdf
-//     pdf.text(text, appConfig.pdf.printMargin, appConfig.pdf.printMargin+5)
-//     pdf.svg(svgToExport,{x:5,y:5,width:287,height:200}).then(() => {
-//       page = page+1
-//       if (page !== pages) {
-//         // iterate
-//         pdf.addPage({orientation:'l', format:'a4'})
-//         addNextSvgToPdf(pdf, page, pages, svgsToExport, textToExport, saveCode)
-//       } else {
-//         addSaveCodeToPdf(pdf, saveCode)
-//         pdf.save('tarsia.pdf')
-//       }
-//     })
-//   }
-//   const saveCode = generateSaveCode()
-//   const previewSvg = document.getElementById('previewSvg')
-//   const printSvgs = document.getElementById('printSvgDiv').children
-//   var svgsToExport = []
-//   var textToExport = []
-//   svgsToExport.push(previewSvg)
-//   textToExport.push('Solution:')
-//   for (const printSvg of printSvgs) {
-//     svgsToExport.push(printSvg)
-//     textToExport.push('Print and cut out:')
-//   }
-//   const svgPages = svgsToExport.length
-//   addNextSvgToPdf(pdf, 0, svgPages, svgsToExport, textToExport, saveCode)
-// };
+// Export to PDF
+const generateAndSavePdf = (saveCode, previewSvg, printSvgs) => {
+  //Initialise pdf
+  const pdf = new jsPDF({
+    orientation: 'landscape',
+    unit:'mm'
+  });
+  const addSaveCodeToPdf = (pdf, saveCode) => {
+    const formattedSaveCode = pdf.splitTextToSize(saveCode, appConfig.pdf.width-appConfig.pdf.printMargin*2)
+    pdf.addPage({orientation:'l', format:'a4'})
+    pdf.text(
+      'To edit your tarsia, go to www.tarsiamaker.co.uk, click load and paste this code:',
+      appConfig.pdf.printMargin, //x
+      appConfig.pdf.printMargin + 5  //y
+    )
+    pdf.text(
+      formattedSaveCode,
+      appConfig.pdf.printMargin, //x
+      appConfig.pdf.printMargin + 15  //y
+    )
+  }
+  const addNextSvgToPdf = (pdf, page, pages, svgsToExport, textToExport, saveCode) => {
+    // Recursive, adds an svg, waits for it to finish, then adds the next one until saving
+    // Get svg
+    let svgToExport = svgsToExport[page]
+    let text = textToExport[page]
+    // Add to pdf
+    pdf.text(text, appConfig.pdf.printMargin, appConfig.pdf.printMargin+5)
+    pdf.svg(svgToExport,{x:5,y:5,width:287,height:200}).then(() => {
+      page = page+1
+      if (page !== pages) {
+        // iterate
+        pdf.addPage({orientation:'l', format:'a4'})
+        addNextSvgToPdf(pdf, page, pages, svgsToExport, textToExport, saveCode)
+      } else {
+        addSaveCodeToPdf(pdf, saveCode)
+        pdf.save('tarsia.pdf')
+      }
+    })
+  }
 
-export {generateSaveCode, parseSaveCode};
+  var svgsToExport = []
+  var textToExport = []
+  svgsToExport.push(previewSvg)
+  textToExport.push('Solution:')
+  for (const printSvg of printSvgs) {
+    svgsToExport.push(printSvg)
+    textToExport.push('Print and cut out:')
+  }
+  const svgPages = svgsToExport.length
+  addNextSvgToPdf(pdf, 0, svgPages, svgsToExport, textToExport, saveCode)
+};
+
+export {
+  generateSaveCode, 
+  parseSaveCode, 
+  generateAndSavePdf
+};
