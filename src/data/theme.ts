9af41e47e0d5db25fb0dc435989279c7fc987803
Peter Graham <peter.graham@15gifts.com>
2021-10-10 00:32:16 +0100

    colours in theme file

--- src/data/theme.ts
+++ src/data/theme.ts
@@ -3,12 +3,19 @@ import type { Theme } from 'theme-ui'
 export const theme: Theme = {
   fonts: {
     body: 'system-ui, sans-serif',
-    heading: '"Avenir Next", sans-serif',
+    heading: '"Helvetica", sans-serif',
     monospace: 'Menlo, monospace',
   },
   colors: {
-    text: '#000',
-    background: '#fff',
-    primary: '#33e',
+    bgDark: '#4c5355',
+    bgLight: '#cbcdcb',
+    textLight: '#eee',
+    textDark: '#222',
+    accent1: '#c44b4f',
+    accent1Dark:'#AB3236',
+    accent2: '#607d86',
+    // text: '#000',
+    // background: '#fff',
+    // primary: '#33e',
   },
 }
