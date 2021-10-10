09233a168fac01b8dce9f2e88af0d5e31f279fa4
Peter Graham <peter.graham@15gifts.com>
2021-10-10 20:13:27 +0100

    adding text splitting functions

--- src/utils/grid.js
+++ src/utils/grid.js
@@ -14,6 +14,49 @@ const calculateGridParameters = (grid) => {
   }
 }
 
+// Handling multi-line
+const smartSplitByNChars = (text, n) => {
+  if (text && text.length>n) {
+    try {
+      const r = new RegExp('(^|\\s).{0,'+(n+1)+'}$');
+      const matchedString = text.match(r)[0].trim()
+      const remainingString = text.replace(r,'')
+      return [remainingString, matchedString]
+    } catch {
+      // If the regex fails, resort to n characters
+      const matchedString = text.substring(text.length - n);
+      const remainingString = text.substring(0, text.length - n) + '-';
+      return [remainingString, matchedString]
+    }
+  }
+  return [text]
+}
+const splitUpText = (text, triangleTextConfig) => {
+  // if shorter than 1st line, don't split it up
+  let textArray = [text]
+  if (text && text.length > triangleTextConfig.style.lineLength[0]) {
+    // Split with regexp
+    var i;
+    var splitText;
+    for(i=0; i<triangleTextConfig.maxLines; i++) {
+      let lineLength = triangleTextConfig.style.lineLength[i]
+      let textToSplit = textArray.shift()
+      if (textToSplit.length <= lineLength) {
+        // if text length less than line length, no need to split it up
+        splitText = [textToSplit]
+        textArray = splitText.concat(textArray)
+        return textArray
+      } else {
+        splitText = smartSplitByNChars(textToSplit, lineLength)
+        textArray = splitText.concat(textArray)
+      }
+    }
+  }
+  return textArray
+}
+
 export {
-  calculateGridParameters
+  calculateGridParameters,
+  smartSplitByNChars,
+  splitUpText
 }
