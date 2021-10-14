2e6daa52ecc9c47e6e5a66611bc9e97fde556b30
Peter Graham <peter.graham@15gifts.com>
2021-10-14 15:45:40 +0100

    input -> svg -> replicated svg somewhere else

--- src/components/CoreMaths/index.js
+++ src/components/CoreMaths/index.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import { Helmet } from 'react-helmet';
 import {appConfig} from '../../data/config';
+import DOMPurify from 'dompurify';
 
 /** @jsx jsx */
 import { ThemeProvider, jsx } from 'theme-ui'
@@ -8,7 +9,9 @@ import { Button, Flex,  Container, Heading, Paragraph, Input } from 'theme-ui'
 import { theme } from '../../data/theme'
 
 // Components
-import { MathComponent } from 'mathjax-react'
+// import { MathJaxContext, MathJax } from 'better-react-mathjax'
+// import { MathJaxSvg } from 'react-native-mathjax-html-to-svg';
+import Tex2SVG from "react-hook-mathjax";
 import Questions from '../QuestionAnswer'
 import grids from '../../data/grids';
 import PrintableSvgDiv from '../PrintableSvgDiv'
@@ -18,6 +21,7 @@ import GridIcon from '../GridIcon'
 import {ClearModal, LoadModal, SaveModal} from '../Modal'
 
 // Functions
+import reactElementToJSXString from 'react-element-to-jsx-string';
 import {generateSaveCode, parseSaveCode, generateAndSavePdf} from '../../utils/saveLoadExport'
 import {calculateGridParameters} from '../../utils/grid'
 
@@ -26,7 +30,6 @@ const ManagedMathsInput = ({name, onChange, ...props}) => {
   const handleInputChange = (event) => {
     setValue(event.target.value);
     onChange(event.target.value);
-    // MathJax.typeset()
   }
   return (
     <Input
@@ -40,57 +43,64 @@ const ManagedMathsInput = ({name, onChange, ...props}) => {
   )
 }
 
-const MathsSvg = ({tex}) => {
-  const [reason, setReason] = useState('')
-  
-  const onError = (reason) => {
-    // reflect the TeX compile error in the component state
-    setReason(reason)
-  }
-  const onSuccess = () => {
-    // clear the reason when we have no errors
-    setReason('')
-  }
-
-  console.log(tex)
+const Wrapper = ({svg}) => {
   return (
-    <MathComponent tex={tex} onError={(err) => onError(err)} onSuccess={() => onSuccess()} />
+    svg
   )
 }
 
 const CoreMaths = (id) => {
   const [maths, setMaths] = useState('')
+  const [mathSvg, setMathSvg] = useState(<svg/>)
+
+  const onSuccess = (html) => {
+    // console.log('html')
+    // console.log(html)  
+    console.log('Child Svg:')
+    var child = html.children[0] // accessing the child
+    console.log(child)
+    // Converting to a nice html string
+    console.log('React element to JSZ string:')
+    // var htmlString = reactElementToJSXString(child)
+    var s = new XMLSerializer(); // convert element / object to xml string
+    var htmlString = s.serializeToString(child);
+    console.log(htmlString)
+    // Sanitising
+    var cleanHtmlString = DOMPurify.sanitize(htmlString)
+    console.log(cleanHtmlString)
+    // Updating State
+    setMathSvg(htmlString)
+    console.log('--stateUpdated--')
+    console.log('--------')
+  }
 
   return (
     <ThemeProvider theme={theme}>
-      <Helmet>
-        <title>Tarsia Maker | Maths</title>
-        {/* Meta Tags */}
-        <meta property='description' content='A simple, online editor for Tarsia puzzles.' />
-        <meta property='theme-color' content="#607d86" />
-        <meta property='title' content='Tarsia Maker | Maths' />
-        <meta property='og:title' content='Tarsia Maker |  Maths' />
-        <meta property='og:type' content='website' />
-        <meta property='og:url' content='https://www.tarsiamaker.co.uk/maths' />
-        <meta property='og:description' content='A simple, online editor for mathematical Tarsia puzzles.' />
-        <meta property='og:image' content='https://i.postimg.cc/MTnhLVH3/preview-image.png' />
-        {/* Scripts */}
-        {/* <script type="text/javascript" src='./mathJaxConfig.js'></script> */}
-        {/* <script type="text/javascript" src='./mathJaxConfig.js'>
-        </script>
-        <script type="text/javascript" id="MathJax-script" async
-          src='https://cdn.jsdelivr.net/npm/mathjax@3/es5/startup.js'>
-        </script> */}
-      </Helmet>
+      {/* <MathJaxContext {...mathJaxConfig}> */}
+        <Helmet>
+          <title>Tarsia Maker | Maths</title>
+          {/* Meta Tags */}
+          <meta property='description' content='A simple, online editor for Tarsia puzzles.' />
+          <meta property='theme-color' content="#607d86" />
+          <meta property='title' content='Tarsia Maker | Maths' />
+          <meta property='og:title' content='Tarsia Maker |  Maths' />
+          <meta property='og:type' content='website' />
+          <meta property='og:url' content='https://www.tarsiamaker.co.uk/maths' />
+          <meta property='og:description' content='A simple, online editor for mathematical Tarsia puzzles.' />
+          <meta property='og:image' content='https://i.postimg.cc/MTnhLVH3/preview-image.png' />
+        </Helmet>
 
-      <Container variant='header'>
-        <Heading>Tarsia Maker | <MathsSvg tex='Maths'/></Heading>
-      </Container>
-      <form key='formy'>
-        <ManagedMathsInput key='mathsInput1-overall' name='mathsInput1' onChange={(data) => setMaths(data)}/>
-      </form>
-      <MathsSvg tex={maths}/>
-
+        <Container variant='header'>
+          <Heading>Tarsia Maker | <Tex2SVG display='inline' latex={'\\text{Maths}'}/></Heading>
+        </Container>
+        <form key='formy'>
+          <ManagedMathsInput key='mathsInput1-overall' name='mathsInput1' onChange={(data) => setMaths(data)}/>
+        </form>
+        <Tex2SVG latex={maths} id='svgOutputContainer' onSuccess={(html) => {onSuccess(html)}} />
+        <div dangerouslySetInnerHTML={{'__html':mathSvg}}/>
+        <svg>
+          <polygon points={`25,0 50,50 0,50`}/>
+        </svg>
     </ThemeProvider>
   );
 }
